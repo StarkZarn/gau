56d766f26720897cdd5914b541617f1171f3e1b2
samz <samz@quantum.localdomain>
2024-05-09 14:15:35 -0700

    point to this repo

--- cmd/gau/main.go
+++ cmd/gau/main.go
@@ -6,9 +6,9 @@ import (
 	"os"
 	"sync"
 
-	"github.com/lc/gau/v2/pkg/output"
-	"github.com/lc/gau/v2/runner"
-	"github.com/lc/gau/v2/runner/flags"
+	"github.com/starkzarn/gau/v2/pkg/output"
+	"github.com/starkzarn/gau/v2/runner"
+	"github.com/starkzarn/gau/v2/runner/flags"
 	log "github.com/sirupsen/logrus"
 )
 
